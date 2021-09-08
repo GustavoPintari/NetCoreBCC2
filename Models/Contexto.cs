17fbde4053cec29c45197e1db923e338db7f8de9
GustavoPintari <gustavopintari@gmail.com>
2021-09-08 13:35:28 -0300

    Construção da classe Contexto

---
+++ Models/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCoreBCC2.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Disciplina> Disciplinas { get; set; }
+        public DbSet<Nota> Notas { get; set; }
+        public DbSet<Professor> Professores { get; set; }
+        public DbSet<Serie> Series { get; set; }
+
+    }
+}
